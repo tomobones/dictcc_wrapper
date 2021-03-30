1a67055ce6c36e1d0d5f5261836400df2f184843
Thomas Vogg <thomas.vogg@posteo.de>
2021-03-30 16:58:23 +0200

    Separated http communication.

--- src/web_client.c
+++ src/web_client.c
@@ -5,127 +5,56 @@
 #include <regex.h>
 #include <string.h>
 
-#include "data_center.h"
+#include "web_client.h"
+#include "exit_err.h"
 
 #define LANG_DEFAULT "deen"
 #define URL_SEARCH "https://%s.dict.cc/?s=%s"
 #define URL_LENGTH 256
 #define FILE_COOKIE "/tmp/dictcc_cookie.txt"
-#define FILE_HTML "/tmp/dictcc_html.txt"
-#define BUFFER_MESSAGE 256
 #define BUFFER_ERROR 256
 
-struct MemoryStruct {
-    char *buffer;
-    size_t size;
-} webdata;
 
-// private methods
-void exit_err(char *message);
+// ________________________________________________________________
+// private methods - declarations
+
+void get_html_from_url(char *url);
 void print_web_data(void);
 const size_t data_handler_search(char* buffer, size_t itemsize, size_t nitems, void* ignorethis);
-
-// add to vocab list
-//
-//vocabids="1451186,719355"
-//vocablist="840649"
-//url="https://www.dict.cc/mydict/ajax_add_entries_to_list.php?list_id=$vocablist&entry_ids=$vocabids"
-//curl -vv --cookie cookies.txt $url
+void parse_strings_from_html(char **ids, char **lvocabs, char **rvocabs);
+void print_str_from_to(char *string, int from, int to);
+void regex_match2string(regex_t rx, char** ptstrg);
+void extract_data_from_strings(char *ids, char *lvocabs, char *rvocabs);
 
 
-// login
-//
-//pw=XXXXXX
-//url=https://secure.dict.cc/users/urc_logn.php
-//#cred="is_utf8=1&hinz=voggo&kunz=$pw&ref=https://www.dict.cc/"
-//cred="hinz=voggo&kunz=$pw"
-//curl -X POST -vv --cookie-jar cookies.txt -d $cred $url > output.txt
+// ________________________________________________________________
+// public methods - implementations
 
-
-// serach
-//
-//word=$1
-//url="https://deen.dict.cc/?s=$word"
-//#curl -b cookies.txt https://users.dict.cc/my-account/ > output.txt
-//curl -cookie cookies.txt $url | grep -E "(idArr|c1Arr|c2Arr)"
-
-
-// vocab lists
-//
-//url="https://my.dict.cc/" # get vocab list id
-//curl -vv --cookie cookies.txt $url | grep -E "*my_vocab_lists\['*"
-
-
-// public methods
-
-bool scrape_vocabs_for_search(char* lang, char* search_string) {
-
-    // test arguments
-    bool need_to_free_lang = false;
-    if (search_string == NULL) return false;
-    if (lang == NULL) {
-        lang = (char *)malloc(sizeof(char) * 5);
-        if (lang == NULL) exit_err("Couldn't alloc var 'lang' in 'web_client.c'");
-        strcpy(lang, LANG_DEFAULT); // default languages
-        need_to_free_lang = true;
-    }
-    
-    // generate search url
-    char url[URL_LENGTH];
-    sprintf(url, URL_SEARCH, lang, search_string);
+void get_data_from_url(struct MemoryStruct *webdata, char *url) {
 
     // curl - set and init vars
     char errbuffer[BUFFER_ERROR];
-    webdata.size = 0;
-    webdata.buffer = malloc(1);
-    if (webdata.buffer == NULL) exit_err("Couldn't alloc var 'data.buffer' in 'web_client.c'");
+    webdata->size = 0;
+    webdata->buffer = malloc(1);
+    if (webdata->buffer == NULL) exit_err("Couldn't alloc var 'data.buffer' in 'web_client.c'");
     CURL *curl_handle = curl_easy_init();
     if (!curl_handle) exit_err("Coudn't initialize curl handle.");
 
     // curl - set options
     curl_easy_setopt(curl_handle, CURLOPT_URL, url);
     curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, data_handler_search);
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void*)&webdata);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void*)webdata);
     curl_easy_setopt(curl_handle, CURLOPT_ERRORBUFFER, errbuffer);
 
     // curl - retrieve data
     if (curl_easy_perform(curl_handle) != CURLE_OK) exit_err(errbuffer);
-    
-    // clean up
-    print_web_data();
+
+    // curl - cleanup
     curl_easy_cleanup(curl_handle);
-    if (need_to_free_lang == true) free(lang);
-    return true;
 }
 
-bool login_user(char* user, char* pass) {
-    return false;
-}
-
-bool scrape_vocablists_for_lang(char* lang) {
-    return false;
-}
-
-void add_vocab_to_vocablist(int vocab_id, int list_id) {
-}
-
-void web_client_clean_up(void) {
-    free(webdata.buffer);
-
-    // delete cookie in case
-}
-
-// private methods
-
-void exit_err(char *message) {
-    fprintf(stderr, "Error: %s\n", message);
-    exit(EXIT_FAILURE);
-}
-
-void print_web_data(void) {
-    if (webdata.buffer == NULL) return;
-    printf("\n\n%s\n\n", webdata.buffer);
-}
+// ________________________________________________________________
+// private methods - implementations
 
 const size_t data_handler_search(char* buffer, size_t itemsize, size_t nitems, void* mem_data) {
     int nbytes = (int)itemsize * (int)nitems;
@@ -139,26 +68,3 @@ const size_t data_handler_search(char* buffer, size_t itemsize, size_t nitems, v
 
     return (size_t)nbytes; 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/web_client.h
+++ src/web_client.h
@@ -1,12 +1,11 @@
 #ifndef _WEB_CLIENT_H_
 #define _WEB_CLIENT_H_
 
-void scrape_vocabs_for_search(char* lang, char* search_string);
-void login_user(char* user, char* pass);
-void scrape_vocablists_for_lang(char* lang);
-void add_vocab_to_vocablist(int vocab_id, int list_id);
-void web_client_clean_up(void);
+struct MemoryStruct {
+    char *buffer;
+    size_t size;
+};
 
-void print_url(void);
+void get_data_from_url(struct MemoryStruct *webdata, char *url);
 
 #endif

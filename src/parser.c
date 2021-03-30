d4c0dc73706f0ec59ce3c44a7c4f0b73edadedeb
Thomas Vogg <thomas.vogg@posteo.de>
2021-03-30 20:23:49 +0200

    Full search functionality.

--- src/parser.c
+++ src/parser.c
@@ -8,69 +8,99 @@
 #include "parser.h"
 #include "exit_err.h"
 
+
+// ________________________________________________________________
+// private vars - declaration
+
 typedef struct SubstrPos {
     long start;
     long end;
-} substrpos_t;
+} substr_pos_t;
 
+// ________________________________________________________________
 // private methods - declaration
 
-void set_initial_positions(substrpos_t *pos_id, substrpos_t *pos_lv, substrpos_t *pos_rv, char *html);
-void pos_of_match_in_html(substrpos_t *pos, regex_t *rx, char *html_data);
-void add_next_vocab(vocab_data_t *vocab_to_add, substrpos_t *pos_id, substrpos_t *pos_lv, substrpos_t *pos_rv, char *html);
+void pos_of_match_in_html(substr_pos_t *pos, regex_t *rx, char *html_data);
+void add_next_vocab(vocab_data_t *vocab_to_add, substr_pos_t *pos_id, substr_pos_t *pos_vl, substr_pos_t *pos_vr, char *html);
 
-// to be replaced...
-void regex_match2string(regex_t rx, char** ptstrg, char* webdata);
-void extract_data_from_strings(char *ids, char *lvocabs, char *rvocabs);
-
-// helper
 void print_str_from_to(char *string, int from, int to);
 
 
+// ________________________________________________________________
 // public methods - implementation
 
 bool parse_vocabs_from_html(char* html) {
 
 
     // set initial positions
-    substrpos_t pos_id, pos_lv, pos_rv;
-    set_initial_positions(&pos_id, &pos_lv, &pos_rv, html); 
-
-    // recurive retrieve of data
-    vocab_data_t vocab_to_add;
-    add_next_vocab(&vocab_to_add, &pos_id, &pos_lv, &pos_rv, html);
-    //vocab_add();
-
-    
-    return true;
-}
-
-// private methods - implementation
-
-void set_initial_positions(substrpos_t *pos_id, substrpos_t *pos_lv, substrpos_t *pos_rv, char *html) {
-
-    // compile regex
+    substr_pos_t pos_id, pos_vl, pos_vr;
     regex_t rx;
     char *pat_ids = "var idArr = new Array([^;]*);";
     char *pat_lvs = "var c1Arr = new Array([^;]*);";
     char *pat_rvs = "var c2Arr = new Array([^;]*);";
-
     regcomp(&rx, pat_ids, REG_EXTENDED); 
-    pos_of_match_in_html(pos_id, &rx, html);
+    pos_of_match_in_html(&pos_id, &rx, html);
     regcomp(&rx, pat_lvs, REG_EXTENDED); 
-    pos_of_match_in_html(pos_lv, &rx, html);
+    pos_of_match_in_html(&pos_vl, &rx, html);
     regcomp(&rx, pat_rvs, REG_EXTENDED); 
-    pos_of_match_in_html(pos_rv, &rx, html);
-
+    pos_of_match_in_html(&pos_vr, &rx, html);
     regfree(&rx);
+
+    // loop retrieve of data
+    char *pat_id = "([0-9]+)";
+    char *pat_v = "\x22([^\x22]*)\x22";
+    regex_t rx_id, rx_v;
+    regcomp(&rx_id, pat_id, REG_EXTENDED);
+    regcomp(&rx_v, pat_v, REG_EXTENDED);
+    
+    substr_pos_t pos_rltv;
+    vocab_data_t vocab_to_add;
+    vocab_to_add.nr = 0;
+
+    char aux_str[16];
+    
+    bool run_further = true;
+    while (run_further) {
+        vocab_to_add.nr++;
+        
+        // retrieve id
+        pos_of_match_in_html(&pos_rltv, &rx_id, html + pos_id.start);
+        if (pos_id.start + pos_rltv.start > pos_id.end) break;
+        strncpy(aux_str, html + pos_id.start + pos_rltv.start, pos_rltv.end - pos_rltv.start);
+        vocab_to_add.id = atol(aux_str);
+        pos_id.start += pos_rltv.end;
+
+        // retrieve left vocabulary
+        pos_of_match_in_html(&pos_rltv, &rx_v, html + pos_vl.start);
+        strncpy(vocab_to_add.vl, html + pos_vl.start + pos_rltv.start, pos_rltv.end - pos_rltv.start);
+        vocab_to_add.vl[pos_rltv.end - pos_rltv.start] = '\0';
+        pos_vl.start += (pos_rltv.end + 1);
+
+        // retrieve right vocabulary
+        pos_of_match_in_html(&pos_rltv, &rx_v, html + pos_vr.start);
+        strncpy(vocab_to_add.vr, html + pos_vr.start + pos_rltv.start, pos_rltv.end - pos_rltv.start);
+        vocab_to_add.vr[pos_rltv.end - pos_rltv.start] = '\0';
+        pos_vr.start += (pos_rltv.end + 1);
+
+        // add vocab to data
+        if (vocab_to_add.id == 0) {
+            vocab_to_add.nr--;
+            continue;
+        }
+
+        vocab_add(&vocab_to_add);
+    }
+        
+    regfree(&rx_id);
+    regfree(&rx_v);
+    
+    return true;
 }
 
-void print_str_from_to(char *string, int from, int to) {
-    for (int i = from; i < to; i++) putchar(string[i]);
-    printf("\n");
-}
+// ________________________________________________________________
+// private methods - implementation
 
-void pos_of_match_in_html(substrpos_t *pos, regex_t *rx, char *html_data) {
+void pos_of_match_in_html(substr_pos_t *pos, regex_t *rx, char *html_data) {
     regmatch_t matches[2];
     if(regexec(rx, html_data, 2, matches, 0) == 0) {
         pos->start = matches[1].rm_so;
@@ -81,15 +111,7 @@ void pos_of_match_in_html(substrpos_t *pos, regex_t *rx, char *html_data) {
     }
 }
 
-void add_next_vocab(vocab_data_t *vocab_to_add, substrpos_t *pos_id, substrpos_t *pos_lv, substrpos_t *pos_rv, char *html) {
-    char *pat_id = "([0-9]+)";
-    char *pat_v = "\x22([^\x22]*)\x22";
-    regex_t rx_id, rx_v;
-    regcomp(&rx_id, pat_id, REG_EXTENDED);
-    regcomp(&rx_v, pat_v, REG_EXTENDED);
-
-    j
-    
-    refree(rx_id);
-    refree(rx_v);
+void print_str_from_to(char *string, int from, int to) {
+    for (int i = from; i < to; i++) putchar(string[i]);
+    printf("\n");
 }

51822849342f10620124bb6093a42aa9a2770fde
Thomas Vogg <thomas.vogg@posteo.de>
2021-04-08 22:00:24 +0200

    Simple implementation of displaying the parsed vocab data.

--- src/user_interface.c
+++ src/user_interface.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <stdbool.h>
 
 #include "user_interface.h"
+#include "data_center.h"
 
 // ________________________________________________________________
 // public methods - implementations - vocabs
@@ -13,9 +15,19 @@ void init_ui(void) {
 
 void display_vocabs(void) {
 
-    attrset(A_BOLD);
-    attrset(A_NORMAL);
-    printw("- press any key -\n");
+    int vocab_number = 1;
+    vocab_data_t *vocab = vocab_for_nr(vocab_number);
+    while (vocab != NULL) {
+        attrset((vocab->is_marked) ? A_BOLD : A_NORMAL);
+        printw("%d: ", vocab->nr);
+        attrset(A_NORMAL);
+        printw("%s ", vocab->vl);
+        attrset(A_BOLD);
+        printw(":");
+        attrset(A_NORMAL);
+        printw(" %s\n", vocab->vr);
+        vocab = vocab_for_nr(++vocab_number);
+    }
 
     refresh();
     getchar();

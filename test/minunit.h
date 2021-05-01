179fa35c4a32d529dcb68c2b65403f03cc5cf840
Thomas Vogg <thomas.vogg@posteo.de>
2021-05-01 17:14:28 +0200

    Changed to original code from source in 'README.md'.

--- test/minunit.h
+++ test/minunit.h
@@ -1,10 +1,9 @@
 /* file: minunit.h */
-
 #ifndef MINUNIT_H
 #define MINUNIT_H
 
-#define mu_assert(message, test) if (!(test)) return message;
-#define mu_run_test(test) char *message = test(); tests_run++; if (message) return message;
+#define mu_assert(message, test) do { if (!(test)) return message; } while (0)
+#define mu_run_test(test) do { char *message = test(); tests_run++; if (message) return message; } while (0)
 extern int tests_run;
 
 #endif

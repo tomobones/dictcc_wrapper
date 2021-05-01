949f05d2444df82b8437cf8efc93303ac57ace74
Thomas Vogg <thomas.vogg@posteo.de>
2021-05-01 21:49:38 +0200

    Bugfix: returning valid url string.

--- src/url_manager.c
+++ src/url_manager.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
-#define LANG_DEFAULT "deen"
 #define LANG_BYTES 5
 #define URL_SEARCH "https://%s.dict.cc/?s=%s"
 
-bool url_for_search_and_lang(char* url, char* search, char* lang) {
+bool url_for_search_and_lang(char* url, const char* search, const char* lang) {
     if (search == NULL || url == NULL) return false;
-    sprintf(url, URL_SEARCH, (lang == NULL) ? LANG_DEFAULT : lang, search);
+    char str[256];
+    sprintf(str, URL_SEARCH, lang, search);
+    strcpy(url, str);
     return true;
 }

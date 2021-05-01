cf8f04dfc5f36c19acb3cd223977d8268502f933
Thomas Vogg <thomas.vogg@posteo.de>
2021-05-01 23:43:48 +0200

    Small change in return value of 'url_for_search_and_lang'

--- src/url_manager.c
+++ src/url_manager.c
@@ -3,13 +3,24 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define LANG_BYTES 5
+#include "err_exit.h"
+
 #define URL_SEARCH "https://%s.dict.cc/?s=%s"
 
-bool url_for_search_and_lang(char* url, const char* search, const char* lang) {
-    if (search == NULL || url == NULL) return false;
+bool is_valid_lang_code (const char* lang) {
+    if (sizeof(lang) != 4) return false;
+    if ((lang[0] == 'd' && lang[1] == 'e') ||\
+        (lang[0] == 'e' && lang[1] == 'n') ||\
+        (lang[2] == 'd' && lang[3] == 'e') ||\
+        (lang[2] == 'e' && lang[3] == 'n')) return true;
+    return false;
+}
+
+void url_for_search_and_lang(char* url, const char* search, const char* lang) {
+    if (search == NULL) err_exit("No valid search string");
+    if (url == NULL) err_exit("No valid url");
+    if (!is_valid_lang_code(lang)) err_exit("No valid language code: %s\n", lang);;
     char str[256];
     sprintf(str, URL_SEARCH, lang, search);
     strcpy(url, str);
-    return true;
 }
--- src/url_manager.h
+++ src/url_manager.h
@@ -1,6 +1,6 @@
 #ifndef URL_MANAGER_H
 #define URL_MANAGER_H
 
-bool url_for_search_and_lang(char* url, char* search, char* lang);
+void url_for_search_and_lang(char* url, const char* search, const char* lang);
 
 #endif

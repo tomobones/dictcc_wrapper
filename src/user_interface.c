76d09cebd4a56090cd2013ebd7825a0257209a2f
Thomas Vogg <thomas.vogg@posteo.de>
2021-05-01 20:34:23 +0200

    Added locale german statically.

--- src/user_interface.c
+++ src/user_interface.c
@@ -7,11 +7,13 @@
 #include "user_interface.h"
 #include "data_center.h"
 
+#define _XOPEN_SOURCE_EXTENDED 1
+
 // ________________________________________________________________
 // public methods - implementations - vocabs
 
 void init_ui(void) {
-    setlocale(LC_ALL, "");
+    setlocale(LC_CTYPE, "de_DE.UTF-8");
     initscr();
 }
 
@@ -20,7 +22,8 @@ void display_vocabs(void) {
     int vocab_number = 1;
     vocab_data_t *vocab = vocab_for_nr(vocab_number);
     while (vocab != NULL) {
-        attrset((vocab->is_marked) ? A_BOLD : A_NORMAL);
+        //attrset((vocab->is_marked) ? A_BOLD : A_NORMAL);
+        attrset(A_BOLD);
         printw("%d: ", vocab->nr);
         attrset(A_NORMAL);
         printw("%s ", vocab->vl);
@@ -30,9 +33,10 @@ void display_vocabs(void) {
         printw(" %s\n", vocab->vr);
         vocab = vocab_for_nr(++vocab_number);
     }
+
     attrset(A_NORMAL);
     printw("\nPress any key...");
-
+    
     refresh();
     getchar();
 }

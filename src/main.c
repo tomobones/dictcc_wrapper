9ae3748ab9d6fd0fd552cf185fd92bf2a2242d98
Thomas Vogg <thomas.vogg@posteo.de>
2021-05-13 21:56:36 +0200

    Added flow to the main file.

--- src/main.c
+++ src/main.c
@@ -16,13 +16,29 @@
 
 char *help_text = \
     "\n"
-    "Options for dictcc\n\n"
-    "\tsearch string without dash '-', several words separated by blanks\n"
-    "-h\thelp\n"
-    "-l\tparameter language codes: combinations of two country codes\n"
-    "\tfor example 'deen', 'defr', 'enru', must include either 'de' or 'en'\n"
-    "\tsupported country codes:\n"
-    "\tde en sv is ru ro fr it sk pt nl hu fi la es bg hr no cs da tr pl eo sr el bs\n"
+    "--------------------------------------------------\n"
+    "               HELP TEXT - DICTCC\n"
+    "--------------------------------------------------\n"
+    "\n"
+    "This is an inofficial client for www.dict.cc\n"
+    "Add search strings to the command without dash '-',\n"
+    "several words are separated by blanks\n"
+    "\n"
+    "Options:\n"
+    "'-h' displays this help text\n"
+    "'-l <language code>' specifies laguages\n"
+    "\n"
+    "the language code is a combinations of two country codes\n"
+    "default value is 'deen' for translation german <> english\n"
+    "must include either 'de' or 'en'\n"
+    "supported country codes are\n"
+    "de en sv is ru ro fr it sk pt nl hu fi\n"
+    "la es bg hr no cs da tr pl eo sr el bs\n"
+    "\n"
+    "examples:\n"
+    "'dictcc car race'\n"
+    "'dictcc -l defr bicyclette'\n"
+    "'dictcc -l enru bicycle',\n"
     "\n";
 
 int main(int argc, char** argv) {
@@ -30,11 +46,10 @@ int main(int argc, char** argv) {
     // manage options
     bool option_help = false;
     bool option_lang = false;
-
     char lang_code[BUFFER_MAX] = "";
-
     int option;
-
+    
+    if (argc == 1) option_help = true;
     while ((option = getopt(argc, argv, "l:h")) != -1) {
         switch(option) {
             case 'h':
@@ -68,7 +83,7 @@ int main(int argc, char** argv) {
     
     // get url
     char url[BUFFER_MAX];
-    url_for_search_and_lang(url, search_string , option_lang?lang_code:LANG_DEFAULT);
+    url_search_and_lang(url, search_string , option_lang?lang_code:LANG_DEFAULT);
 
     // get html data
     struct MemoryStruct memory;
@@ -79,12 +94,38 @@ int main(int argc, char** argv) {
     // parse data
     parse_vocabs_from_html(memory.buffer);
 
-    // display
+    // user interface
     init_ui();
-    display_vocabs();
-    cleanup_ui();
+    if (loop_vocabs_ui()) {
+        
+        // query username and password
+        char username[BUFFER_MAX];
+        char password[BUFFER_MAX];
+        get_user_passwd(username, password);
+        
+        open_session(username, password);
+
+        // read lists from account
+        struct MemoryStruct mem;
+        mem.size = 0;
+        mem.buffer = malloc(1);
+        get_lists_for_lang(&mem, option_lang?lang_code:LANG_DEFAULT);
+        parse_voclsts_from_html(mem.buffer);
+
+        // display lists, choose one
+        //char* list_id_str = "994698"; //TestTerminal
+        char list_id_str[BUFFER_MAX];
+        get_voclst(list_id_str);
+
+        // add vocabs to list
+        char vocab_ids_str[BUFFER_MAX];
+        vocabs_marked(vocab_ids_str);
+        add_vocabs_to_list(vocab_ids_str, list_id_str);
+        close_session();
+    }
 
     // clean up
+    cleanup_ui();
     data_clean_up();
     free(memory.buffer);
 

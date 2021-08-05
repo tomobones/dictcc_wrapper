d869e7edbcb0d9e96031b2181f1b6e409160e563
study <johnstandish9@hotmail.com>
2021-08-05 21:06:47 +0100

    Formatting changes

--- test/data_center_test.c
+++ test/data_center_test.c
@@ -6,6 +6,13 @@
 #include "../include/data_center.h"
 
 int tests_run = 0;
+/* Created two function pointers to static functions which were failing to
+ * compile because they were static. I have limited their visibility to just
+ * this unit test by only including them if TEST_DATA_CENTER is defined. Also 
+ * By doing it this way we avoid duplicating code
+ */
+extern bool (*fp_voclst_exists)(int);
+extern bool (*fp_vocab_exists)(int);
 
 char *test_vocab_add(void) {
     vocab_data_t *data1 = (vocab_data_t*) malloc(sizeof(vocab_data_t));;
@@ -15,7 +22,8 @@ char *test_vocab_add(void) {
     strcpy(data1->vr, "Hallo");
     data1->is_marked = false;
     
-    mu_assert("vocab_add: added 1st vocab, should return true", vocab_add(data1));
+    mu_assert("vocab_add: added 1st vocab, should return true",
+              vocab_add(data1));
 
     vocab_data_t *data2 = (vocab_data_t*) malloc(sizeof(vocab_data_t));;
     data2->nr = 2;
@@ -24,7 +32,8 @@ char *test_vocab_add(void) {
     strcpy(data2->vr, "Hallo");
     data2->is_marked = false;
 
-    mu_assert("vocab_add: added 2nd vocab, should return true", vocab_add(data2));
+    mu_assert("vocab_add: added 2nd vocab, should return true",
+              vocab_add(data2));
 
     return 0;
 }
@@ -36,12 +45,12 @@ char *test_vocab_add_redundancy(void) {
     strcpy(data1->vl, "hello");
     strcpy(data1->vr, "Hallo");
     data1->is_marked = false;
-    mu_assert("vocab_add: redundant id should return false", !vocab_add(data1));
+    mu_assert("vocab_add: redundant id should return false",
+              !vocab_add(data1));
 
     return 0;
 }
 
-extern bool (*fp_vocab_exists)(int);
 char *test_vocab_exists(void) {
     /* Calling private function so declaring it here. */
 
@@ -59,11 +68,11 @@ char *test_vocab_for_nr() {
     int id = 234567;
     int number = 1;
     vocab_data_t *data = vocab_for_nr(number);
-    mu_assert("vocab_for_nr: shoud return data with adaquate id", data->id == id);
+    mu_assert("vocab_for_nr: shoud return data with adaquate id",
+              data->id == id);
 
     return 0;
 }
-
 
 char *test_voclst_add(void) {
     voclst_data_t lstdata1; 
@@ -86,17 +95,13 @@ char *test_voclst_add_redundancy(void) {
     lstdata3.nr = 3;
     lstdata3.id = 1999;
     strcpy(lstdata3.name, "japanese_list");
-    mu_assert("voclst_add: redundant id should return false.", !voclst_add(&lstdata3));
+    mu_assert("voclst_add: redundant id should return false.",
+              !voclst_add(&lstdata3));
 
     return 0;
 }
 
-extern bool (*fp_voclst_exists)(int);
-
 char *test_voclst_exists(void) {
-    /* Calling prave function so declaring it here. */
- //   bool voclst_exists(int);
-
     int id = 1999;
     int id_not = 2000;
     mu_assert("voclst_exists: for existing id should return true",
@@ -111,7 +116,8 @@ char *test_voclst_for_nr(void) {
     int lnr = 2;
     int id = 2999;
     voclst_data_t *data = voclst_for_nr(lnr);
-    mu_assert("vocab_for_nr: should return data with adaquate id", data->id == id);
+    mu_assert("vocab_for_nr: should return data with adaquate id",
+              data->id == id);
 
     return 0;
 }
@@ -135,7 +141,8 @@ int main(int argc, char** argv) {
     printf("\nTest: %s\n", argv[0]);
     char *test_message = all_tests();
     if (test_message != 0) printf("Test failed. Msg: %s\n", test_message);
-    else printf("All tests in passed! (%i total test%c)\n\n", tests_run, (tests_run==1)?0:'s');
+    else printf("All tests in passed! (%i total test%c)\n\n", tests_run,
+                (tests_run==1)?0:'s');
 
     return 0;
 }
